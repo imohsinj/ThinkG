c36c8aeef61bec4c5df6b94165e4529e3af7f87c
Mohsin Jamadar <not4mj@gmail.com>
2017-04-11 13:03:39 +0530

    code Commit

---
+++ Think Golf/Libs/Common/SegueIdentifiers.swift
@@ -0,0 +1,17 @@
+//
+//  SegueIdentifiers.swift
+//  MCHAnywherePhysician
+//
+//  Created by Mohsin Jamadar on 12/4/15.
+//  Copyright © 2015 Lester Moreno. All rights reserved.
+//
+
+import Foundation
+
+class SegueIdentifiers {
+    
+    static let presentDashboard = "PresentDashboardSegueId"
+    static let unwindSignUpToLogin = "UnwindSignUpToLogin"
+    static let dissmisLinkedIn = "idSegueShowWebVC"
+    static let openMenuIdentifier = "OpenMenuIdentifier"
+}
